38dd53f6e1b662515fa697c1c6cc64b11f831c8d
tayanarapaiva <45939697+tayanarapaiva@users.noreply.github.com>
2019-02-25 09:38:40 -0300

    Update registrador_anel.vhd

--- Minicurso_FPGA-VHDL-master/registrador_anel.vhd
+++ Minicurso_FPGA-VHDL-master/registrador_anel.vhd
@@ -1,8 +1,7 @@
 -- Registrador Anel 1 byte
 -- Entradas: in_paralelo (std_logic_vector, 8 posições), in_clk (std_logic, entrada do clock)
 -- Saídas: out_clk (std_logic, saída 1 segundo), out_paralelo(std_logic_vector, 8 posições)
--- Modificado por: EDMILA DE MACEDO
--- Data: 23/01/2019
+-- Modificado por: tayanara paiva
  
  
 library ieee;
